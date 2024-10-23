21afd8dbe2ed21a52e5d403faf2c869a192a7678
Kaiden Buchter <knbuchtyy879@gmail.com>
2024-10-23 14:18:14 +0000

    Refactor chatbot response output in MyClass

--- MyClass.java
+++ MyClass.java
@@ -5,17 +5,17 @@ public class MyClass {
         Magpie chatbot = new Magpie();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Bot: " + chatbot.getGreeting());
+        System.out.println("Magpie: " + chatbot.getGreeting());
 
         while (true) {
             System.out.print("You: ");
             String userInput = scanner.nextLine().trim();
-            if (userInput.equalsIgnoreCase("exit")) {
-                System.out.println("Bot: Goodbye!");
+            if (userInput.equalsIgnoreCase("bye") || userInput.equalsIgnoreCase("goodbye")) {
+                System.out.println("Magpie: Goodbye!");
                 break;
             }
             String response = chatbot.getResponse(userInput);
-            System.out.println("Bot: " + response);
+            System.out.println("Magpie: " + response);
         }
 
         scanner.close();
